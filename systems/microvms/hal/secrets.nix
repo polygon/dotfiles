b77287d48bbf2cf7b3c49f17fc7d4cacccf844fb
polygon <polygon@wh2.tu-dresden.de>
2022-06-19 23:31:56 +0200

    Fix permissions

--- systems/microvms/hal/secrets.nix
+++ systems/microvms/hal/secrets.nix
@@ -3,10 +3,17 @@
   sops.defaultSopsFile = "${secrets}/hosts/hal/secrets.yaml";
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.gnupg.sshKeyPaths = [ ];
-  sops.secrets = {
+  sops.secrets = let
+    permissions = {
+      owner = "mosquitto";
+      group = "mosquitto";
+      mode = "0440";
+    };
+  in
+  {
     mosquitto-users-admin = { };
     mosquitto-users-plugs = { };
-    mosquitto-bridge-pi-username = { };
-    mosquitto-bridge-pi-password = { };
+    mosquitto-bridge-pi-username = permissions;
+    mosquitto-bridge-pi-password = permissions;
   };
 }
