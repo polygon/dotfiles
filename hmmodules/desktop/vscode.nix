981e13220dcfd90ce0524e4894b7f7d525e3ff71
polygon <polygon@wh2.tu-dresden.de>
2023-10-28 16:40:23 +0200

    VSCode

--- hmmodules/desktop/vscode.nix
+++ hmmodules/desktop/vscode.nix
@@ -11,7 +11,7 @@ in
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      package = pkgs.vscodium;
+      #package = pkgs.vscodium;
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
         vadimcn.vscode-lldb
@@ -19,30 +19,30 @@ in
         ms-vscode-remote.remote-ssh
         ms-python.python
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "command-server";
-          publisher = "pokey";
-          version = "0.8.2";
-          sha256 = "sha256-mVDXn8jygpmshr7Xxve57JV3UMci3oeeLHr5dWirkOw=";
-        }
-        {
-          name = "cursorless";
-          publisher = "pokey";
-          version = "0.26.495";
-          sha256 = "sha256-VR1LMA86WRszU/66eP+aH7iAm4yxsMHhMPCIWFtYJfc=";
-        }
-        {
-          name = "parse-tree";
-          publisher = "pokey";
-          version = "0.23.0";
-          sha256 = "sha256-sFsYoHcwctesCzD5ItfnRPEop3WPV1eSvl642FcgzPk=";
-        }
-        {
-          name = "talon";
-          publisher = "pokey";
-          version = "0.2.0";
-          sha256 = "sha256-BPc0jGGoKctANP4m305hoG9dgrhjxZtFdCdkTeWh/Xk=";
-        }
+        #{
+        #  name = "command-server";
+        #  publisher = "pokey";
+        #  version = "0.8.2";
+        #  sha256 = "sha256-mVDXn8jygpmshr7Xxve57JV3UMci3oeeLHr5dWirkOw=";
+        #}
+        #{
+        #  name = "cursorless";
+        #  publisher = "pokey";
+        #  version = "0.26.495";
+        #  sha256 = "sha256-VR1LMA86WRszU/66eP+aH7iAm4yxsMHhMPCIWFtYJfc=";
+        #}
+        #{
+        #  name = "parse-tree";
+        #  publisher = "pokey";
+        #  version = "0.23.0";
+        #  sha256 = "sha256-sFsYoHcwctesCzD5ItfnRPEop3WPV1eSvl642FcgzPk=";
+        #}
+        #{
+        #  name = "talon";
+        #  publisher = "pokey";
+        #  version = "0.2.0";
+        #  sha256 = "sha256-BPc0jGGoKctANP4m305hoG9dgrhjxZtFdCdkTeWh/Xk=";
+        #}
         {
           name = "vscode-direnv";
           publisher = "cab404";
