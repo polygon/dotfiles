e7d553315afb7bc202e5deafa660f21e12fc3b97
polygon <polygon@wh2.tu-dresden.de>
2026-09-12 21:30:08 +0200

    Fix renewal

--- systems/nubego/acme.nix
+++ systems/nubego/acme.nix
@@ -6,7 +6,7 @@
     certs."nubego.de" = {
       dnsProvider = "cloudflare";
       dnsResolver = "1.1.1.1:53";
-      credentialFiles."CLOUDFLARE_DNS_API_TOKEN_FILE" = "/run/secrets/cloudflare-dns-api-token";
+      environmentFile = "/run/secrets/cloudflare-dns-api-token";
       dnsPropagationCheck = true;
       domain = "nubego.de";
       extraDomainNames = [ "*.nubego.de" ];
